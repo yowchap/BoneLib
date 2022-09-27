f031e1b2cd9e990534bdb0a09cff928127000b69
yowchap <50385582+yowchap@users.noreply.github.com>
2022-09-26 17:35:46 -0700

    Added HelperMethods class from MTINM

---
+++ BoneLib/BoneLib/HelperMethods.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace BoneLib
+{
+    public static class HelperMethods
+    {
+        /// <summary>
+        /// Removes things like [2] and (Clone)
+        /// </summary>
+        public static string GetCleanObjectName(string name)
+        {
+            Regex regex = new Regex(@"\[\d+\]|\(\d+\)"); // Stuff like (1) or [24]
+            name = regex.Replace(name, "");
+            name = name.Replace("(Clone)", "");
+            return name.Trim();
+        }
+    }
+}
