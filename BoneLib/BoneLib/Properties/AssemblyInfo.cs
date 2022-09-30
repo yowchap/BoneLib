93294ebc52782e8f371b0ea636a3b9787a1c18e5
yowchap <50385582+yowchap@users.noreply.github.com>
2022-09-30 16:15:54 -0700

    Added ML and game references

--- BoneLib/BoneLib/Properties/AssemblyInfo.cs
+++ BoneLib/BoneLib/Properties/AssemblyInfo.cs
@@ -22,4 +22,4 @@ using System.Runtime.InteropServices;
 // Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
 // If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
 // Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
-[assembly: MelonGame("Stress Level Zero", "BONEWORKS")]
+[assembly: MelonGame("Stress Level Zero", "BONELAB")]
