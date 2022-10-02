cb94f8f1c8007ddc90355e461697a9c55937c64f
L4rs <lars.ijntema@ziggo.nl>
2022-10-02 21:03:10 +0200

    First code for UI (I hope GetElementsOfType actually works)

---
+++ BoneLib/BoneLib/UI/Element.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace BoneLib.UI
+{
+    public class Element
+    {
+        public Vector2 Position;
+        public Color FrontColor;
+    }
+}
---
+++ BoneLib/BoneLib/UI/Page.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoneLib.UI
+{
+    public class Page
+    {
+        public string Title { get; set; }
+        public List<Element> Elements { get; set; }
+
+        public Page(string title)
+        {
+            Title = title;
+        }
+
+        public Element[] GetElementsOfType<T>(T elementType) where T : Element
+        {
+            return Elements.Where(e => e.GetType() == elementType.GetType()).ToArray();
+        }
+
+        public void AddElement(Element e) => Elements.Add(e);
+
+        public void RemoveElement(Element e) => Elements.Remove(e);
+    }
+}
