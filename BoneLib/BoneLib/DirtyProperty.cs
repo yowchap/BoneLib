e1a4dd138ef201b231e9bc6d9b95f1bcce8fc975
yowchap <50385582+yowchap@users.noreply.github.com>
2022-10-10 16:08:54 -0700

    Cleanup

--- BoneLib/BoneLib/DirtyProperty.cs
+++ BoneLib/BoneLib/DirtyProperty.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-
-namespace BoneLib
+﻿namespace BoneLib
 {
     public class DirtyProperty<T>
     {
