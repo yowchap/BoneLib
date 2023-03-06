f4782a08dd04f96c13769f9a461f154b9ae435be
yowchap <50385582+yowchap@users.noreply.github.com>
2023-03-05 22:19:04 -0800

    Fixed updater to hopefully work with mod manager

--- BoneLib/BoneLibUpdater/Main.cs
+++ BoneLib/BoneLibUpdater/Main.cs
@@ -9,7 +9,7 @@ namespace BoneLibUpdater
         public const string Name = "BoneLibUpdater"; // Name of the Mod.  (MUST BE SET)
         public const string Author = "Gnonme"; // Author of the Mod.  (Set as null if none)
         public const string Company = null; // Company that made the Mod.  (Set as null if none)
-        public const string Version = "1.1.0"; // Version of the Mod.  (MUST BE SET)
+        public const string Version = "1.1.1"; // Version of the Mod.  (MUST BE SET)
         public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
     }
 
@@ -19,8 +19,8 @@ namespace BoneLibUpdater
         private MelonPreferences_Entry<bool> offlineModePref;
         private bool isOffline => offlineModePref.Value;
 
-        public static readonly string boneLibAssemblyPath = Path.Combine(MelonUtils.GameDirectory, "Mods", "BoneLib.dll");
-        public static readonly string boneLibUpdaterAssemblyPath = Path.Combine(MelonUtils.GameDirectory, "Plugins", "BoneLibUpdater.dll");
+        public static readonly string boneLibAssemblyPath = Path.Combine(MelonHandler.ModsDirectory, "BoneLib.dll");
+        public static readonly string boneLibUpdaterAssemblyPath = Path.Combine(MelonHandler.PluginsDirectory, "BoneLibUpdater.dll");
 
         public static MelonLogger.Instance Logger { get; private set; }
 
