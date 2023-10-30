be40399a34ea74546ecf1db4b4e750a89665abc0
CarrionAndOn <maya@carrionandon.me>
2023-10-30 13:17:59 -0700

    SpawnCrate method in HelperMethods.cs

--- BoneLib/BoneLib/HelperMethods.cs
+++ BoneLib/BoneLib/HelperMethods.cs
@@ -1,5 +1,11 @@
-﻿using MelonLoader;
+﻿using System;
+using MelonLoader;
 using System.Text.RegularExpressions;
+using BoneLib.Nullables;
+using SLZ.Marrow.Data;
+using SLZ.Marrow.Pool;
+using SLZ.Marrow.Warehouse;
+using UnityEngine;
 
 namespace BoneLib
 {
@@ -22,5 +28,44 @@ namespace BoneLib
         public static bool IsAndroid() => isAndroid;
 
         private static readonly bool isAndroid = MelonUtils.CurrentPlatform == (MelonPlatformAttribute.CompatiblePlatforms)3;
+
+        /// <summary>
+        /// Spawns a crate from barcode.
+        /// </summary>
+        /// <param name="barcode">The barcode of the crate</param>
+        /// <param name="position">The position to spawn the crate at</param>
+        /// <param name="rotation">The rotation of the spawned object</param>
+        /// <param name="scale">The scale of the spawned object</param>
+        /// <param name="ignorePolicy">Ignore spawn policy or not</param>
+        /// <param name="spawnAction">Code to run once the spawnable is placed</param>
+        public static void SpawnCrate(string barcode, Vector3 position, Quaternion rotation, Vector3 scale, bool ignorePolicy, Action<GameObject> spawnAction)
+        {
+            var crateRef = new SpawnableCrateReference(barcode);
+            var spawnable = new Spawnable()
+            {
+                crateRef = crateRef,
+            };
+            AssetSpawner.Register(spawnable);
+            AssetSpawner.Spawn(spawnable, position, rotation, new BoxedNullable<Vector3>(scale), ignorePolicy,new BoxedNullable<int>(null), spawnAction);
+        }
+        
+        /// <summary>
+        /// Spawns a crate from a crate reference.
+        /// </summary>
+        /// <param name="crateReference">The crate reference to spawn</param>
+        /// <param name="position">The position to spawn the crate at</param>
+        /// <param name="rotation">The rotation of the spawned object</param>
+        /// <param name="scale">The scale of the spawned object</param>
+        /// <param name="ignorePolicy">Ignore spawn policy or not</param>
+        /// <param name="spawnAction">Code to run once the spawnable is placed</param>
+        public static void SpawnCrate(SpawnableCrateReference crateReference, Vector3 position, Quaternion rotation, Vector3 scale, bool ignorePolicy, Action<GameObject> spawnAction)
+        {
+            var spawnable = new Spawnable()
+            {
+                crateRef = crateReference,
+            };
+            AssetSpawner.Register(spawnable);
+            AssetSpawner.Spawn(spawnable, position, rotation, new BoxedNullable<Vector3>(scale), ignorePolicy,new BoxedNullable<int>(null), spawnAction);
+        }
     }
 }
