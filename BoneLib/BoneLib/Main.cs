8f78a7c95bff1c8c4828678b48f6fb5ed700289e
yowchap <50385582+yowchap@users.noreply.github.com>
2023-11-18 11:56:04 -0800

    2.4.0 (#57)
    * update version number
    
    * Update README.md

--- BoneLib/BoneLib/Main.cs
+++ BoneLib/BoneLib/Main.cs
@@ -15,7 +15,7 @@ namespace BoneLib
         public const string Name = "BoneLib"; // Name of the Mod.  (MUST BE SET)
         public const string Author = "Gnonme"; // Author of the Mod.  (Set as null if none)
         public const string Company = null; // Company that made the Mod.  (Set as null if none)
-        public const string Version = "2.3.0"; // Version of the Mod.  (MUST BE SET)
+        public const string Version = "2.4.0"; // Version of the Mod.  (MUST BE SET)
         public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
     }
 
