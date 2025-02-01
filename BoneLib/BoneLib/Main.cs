b319f1d247d6e5fd9652b77162bdd47396f05db8
HAHOOS <hahoos@hahoos.pl>
2025-02-01 21:01:14 +0100

    Bring back SkipIntro (not sure why it was removed in the first place)

--- BoneLib/BoneLib/Main.cs
+++ BoneLib/BoneLib/Main.cs
@@ -2,7 +2,11 @@ using BoneLib.BoneMenu;
 using BoneLib.Notifications;
 using BoneLib.RandomShit;
 
+using Il2CppSLZ.Bonelab;
+
 using MelonLoader;
+
+using UnityEngine;
 
 namespace BoneLib
 {
@@ -53,12 +57,59 @@ namespace BoneLib
 
         private void OnLevelLoaded(LevelInfo info)
         {
+            if (info.barcode == CommonBarcodes.Maps.VoidG114 || info.barcode == CommonBarcodes.Maps.MainMenu)
+                SkipIntro();
+
             PopupBoxManager.CreateBaseAd();
         }
 
         private void OnUIRigCreated()
         {
             MenuBootstrap.InitializeReferences();
+        }
+
+        private void SkipIntro()
+        {
+            if (!Preferences.skipIntro)
+                return;
+
+            GameObject uiRoot = GameObject.Find("CANVAS_UX");
+
+            GameObject slzWobble = uiRoot.transform.Find("SLZ_ROOT").gameObject;
+            GameObject marrowWobble = uiRoot.transform.Find("CREDITS_ROOT").gameObject;
+            GameObject boneBeaker = uiRoot.transform.Find("BONELAB_ANIM").gameObject;
+
+            GameObject.Destroy(slzWobble);
+            GameObject.Destroy(marrowWobble);
+
+            boneBeaker.transform.localPosition = Vector3.up * 0.375f;
+            boneBeaker.transform.localScale = Vector3.one * 0.5f;
+
+            GameObject requiredUI = uiRoot.transform.Find("REQUIRED").gameObject;
+            requiredUI.SetActiveRecursively(true);
+
+            GameObject menuUI = uiRoot.transform.Find("MENU").gameObject;
+            menuUI.gameObject.SetActive(true);
+
+            menuUI.transform.Find("group_Enter").gameObject.SetActive(false);
+            menuUI.transform.Find("group_Options").gameObject.SetActive(false);
+            menuUI.transform.Find("group_Mods").gameObject.SetActive(false);
+            menuUI.transform.Find("group_Info").gameObject.SetActive(false);
+            menuUI.transform.Find("group_BETA").gameObject.SetActive(false);
+
+            GameControl_MenuVoidG114 controller = GameObject.FindObjectOfType<GameControl_MenuVoidG114>();
+
+            if (controller != null)
+            {
+                controller.holdTime = 0;
+                controller.holdTime_SLZ = 0;
+                controller.holdTime_Credits = 0;
+                controller.holdTime_GameTitle = 0;
+                controller.timerHold = 0;
+                controller.holdTime_Rest = 0;
+                controller.canClick = true;
+                controller.fadeVolume.gameObject.SetActive(false); // Set black fog volume to inactive to prevent it never fading out
+            }
         }
     }
 }
