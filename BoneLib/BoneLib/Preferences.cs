29b6b65d9010a13e4dfccf8b2706e91c9aff14fa
sanic <92267915+freakycheesy@users.noreply.github.com>
2026-06-29 16:33:01 +0100

    Update Preferences.cs

--- BoneLib/BoneLib/Preferences.cs
+++ BoneLib/BoneLib/Preferences.cs
@@ -1,4 +1,6 @@
-﻿using MelonLoader;
+﻿using Il2CppSLZ.Marrow;
+using Il2CppSLZ.Marrow.Data;
+using MelonLoader;
 
 namespace BoneLib
 {
@@ -15,6 +17,16 @@ namespace BoneLib
             skipIntro = new ModPref<bool>(category, "SkipIntro", false);
             loggingMode = new ModPref<LoggingMode>(category, "LoggingMode", LoggingMode.NORMAL);
             infiniteAmmo = new ModPref<bool>(category, "InfiniteAmmo", false);
+            if (infiniteAmmo.entry.Value)
+            {
+                AmmoInventory ammoInventory = AmmoInventory.Instance;
+                AmmoGroup[] ammoGroups = {ammoInventory.lightAmmoGroup, ammoInventory.mediumAmmoGroup, ammoInventory.heavyAmmoGroup};
+                string[] ammoCartridges = {"light","medium", "heavy"};
+                for (int i = 0; i < 3; i++)
+                {
+                    if (ammoInventory.GetCartridgeCount(ammoCartridges[i]) < 1) ammoInventory.AddCartridge(ammoGroups[i], short.MaxValue);
+                }
+            }
             Save();
             ModConsole.Msg("Finished preferences setup", LoggingMode.DEBUG);
         }
