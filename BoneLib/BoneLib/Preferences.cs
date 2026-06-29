9ceff24ab3322c88350ec3fc568c484f0cfaa8ce
sanic <92267915+freakycheesy@users.noreply.github.com>
2026-06-29 16:39:24 +0100

    Update Preferences.cs

--- BoneLib/BoneLib/Preferences.cs
+++ BoneLib/BoneLib/Preferences.cs
@@ -17,18 +17,38 @@ namespace BoneLib
             skipIntro = new ModPref<bool>(category, "SkipIntro", false);
             loggingMode = new ModPref<LoggingMode>(category, "LoggingMode", LoggingMode.NORMAL);
             infiniteAmmo = new ModPref<bool>(category, "InfiniteAmmo", false);
+            infiniteAmmo.entry.OnEntryValueChanged.Subscribe((_, value) =>
+            {
+                // Gives Ammo once the preference is toggled on
+                if (value)
+                {
+                    GiveAmmo();
+                }
+            });
             if (infiniteAmmo.entry.Value)
             {
-                AmmoInventory ammoInventory = AmmoInventory.Instance;
-                AmmoGroup[] ammoGroups = {ammoInventory.lightAmmoGroup, ammoInventory.mediumAmmoGroup, ammoInventory.heavyAmmoGroup};
-                string[] ammoCartridges = {"light","medium", "heavy"};
-                for (int i = 0; i < 3; i++)
-                {
-                    if (ammoInventory.GetCartridgeCount(ammoCartridges[i]) < 1) ammoInventory.AddCartridge(ammoGroups[i], short.MaxValue);
-                }
+                GiveAmmo();
             }
             Save();
             ModConsole.Msg("Finished preferences setup", LoggingMode.DEBUG);
+        }
+
+        // Added from hahoos comment
+        internal static void GiveAmmo()
+        {
+            if (infiniteAmmo.entry.Value)
+            {
+                // Gets the ammo inventory
+                AmmoInventory ammoInventory = AmmoInventory.Instance;
+                // Creates an array of groups and carts.
+                AmmoGroup[] ammoGroups = { ammoInventory.lightAmmoGroup, ammoInventory.mediumAmmoGroup, ammoInventory.heavyAmmoGroup };
+                string[] ammoCartridges = { "light", "medium", "heavy" };
+                for (int i = 0; i < 3; i++)
+                {
+                    // Adds cart. if there is no ammo
+                    if (ammoInventory.GetCartridgeCount(ammoCartridges[i]) < 1) ammoInventory.AddCartridge(ammoGroups[i], short.MaxValue);
+                }
+            }
         }
 
         public static void Save()
